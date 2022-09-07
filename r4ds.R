201a59bdccd55ba28979f61b2e5ac973fd918ee5
Tyler Nardone <tynardone@gmail.com>
2022-09-07 14:46:24 -0400

    updated

--- r4ds.R
+++ r4ds.R
@@ -4,7 +4,7 @@ library(modelr)
 
 
 tibble(x = c("a,b,c", "d,e,f,g", "h,i,j")) %>% 
-  separate(x, c("one", "two", "three"))
+  separate(x, c("one", "two", "three"), extra = "merge")
 
 tibble(x = c("a,b,c", "d,e", "f,g,i")) %>% 
-  separate(x, c("one", "two", "three"))
+  separate(x, c("one", "two", "three"), fill = "left")
